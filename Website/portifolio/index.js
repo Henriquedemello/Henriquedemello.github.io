51c5bd41266c7a4908fa4d0d345347f24633c2d4
Henrique de Mello <henriquemello749@gmail.com>
2024-01-25 14:42:34 -0300

    primeiro commit
    primeiro commit do portifólio

---
+++ Website/portifolio/index.js
@@ -0,0 +1,11 @@
+function menuShow() {
+  let menuMobile = document.querySelector(".menu-mobile-links");
+  if (menuMobile.classList.contains("open")) {
+    menuMobile.classList.remove("open");
+    (document.querySelector(".mobile-menu").scr = '<div class="line1"></div>'),
+      '<div class="line2"></div>',
+      '<div class="line3"></div>';
+  } else {
+    menuMobile.classList.add("open");
+  }
+}
